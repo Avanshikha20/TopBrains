c6969fffd9b17eb132b774b378193ca0662bb10c
Avanshikha singh <singhavanshikha@gmail.com>
2026-01-20 12:47:55 +0530

    code

---
+++ MidPointRounding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml;
+class Program
+{
+    public double Area(double radius)
+    {
+        
+        double Area=Math.PI*(radius*radius);
+        return Area;
+    }
+    public static void Main()
+    {
+        Program p=new Program();
+        double input=Convert.ToDouble(Console.ReadLine());
+        double output=p.Area(input);
+        System.Console.WriteLine(Math.Round(output,2));
+    }
+}
