04769b13971c6a5311540beea7c9d1cffc989d55
Avanshikha singh <singhavanshikha@gmail.com>
2026-01-20 15:09:58 +0530

    code

---
+++ LargestInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    public int Largest(int a,int b,int c)
+    {
+        if(a>b && a > c)
+        {
+            return a;
+        }else if(b>c && b > a)
+        {
+            return b;
+        }
+        else
+        {
+            return c;
+        }
+    }
+    public static void Main()
+    {
+        Program p=new Program();
+        int a=Convert.ToInt32(Console.ReadLine());
+        int b=Convert.ToInt32(Console.ReadLine());
+        int c=Convert.ToInt32(Console.ReadLine());
+        int Largestint=p.Largest(a,b,c);
+        System.Console.WriteLine(Largestint);
+    }
+}
