73d6a6588edc76e16635225b059f5446c32d30ee
Avanshikha singh <singhavanshikha@gmail.com>
2026-01-20 12:35:47 +0530

    code

---
+++ Conversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    public double Conversion(int Input)
+    {
+        double Output=Input*30.48;
+        return Output;
+    }
+    public static void Main()
+    {
+        Program p=new Program();
+        int input=Convert.ToInt32(Console.ReadLine());
+        double output=p.Conversion(input);
+        System.Console.WriteLine(Math.Round(output,2));
+
+
+    }
+
+}
