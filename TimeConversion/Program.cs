d83e718ad33ca3ca82716469daed0ae6171d6a94
Avanshikha singh <singhavanshikha@gmail.com>
2026-01-20 15:51:48 +0530

    code

---
+++ TimeConversion/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    public static string TimeConversion(int Tseconds)
+    {
+        int minute=Tseconds/60;
+        int Seconds=Tseconds%60;
+        
+        return minute+":"+Seconds.ToString("D2");
+
+    }
+    public static void Main()
+    {
+        int Seconds=Convert.ToInt32(Console.ReadLine());
+        string output=TimeConversion(Seconds);
+        System.Console.WriteLine(output);
+    }
+}
