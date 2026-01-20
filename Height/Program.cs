bf11c1d2e8290362e6ce4fe514aa45409035cf09
Avanshikha singh <singhavanshikha@gmail.com>
2026-01-20 12:57:27 +0530

    code

---
+++ Height/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class Program
+{
+    public string Category(int height)
+    {
+        if (height < 150)
+        {
+            return "short";
+        }
+        else if(height>=150 && height < 180)
+        {
+            return "Average";
+        }
+        else
+        {
+            return "tall";
+        }
+    }
+    public static void Main()
+    {
+        Program p=new Program();
+        int input=Convert.ToInt32(Console.ReadLine());
+        string output=p.Category(input);
+        System.Console.WriteLine(output);
+    }
+}
