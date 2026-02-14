28652e8a984e28a68f75d94987b8e10ba08a9f65
Avanshikha singh <singhavanshikha@gmail.com>
2026-02-14 12:09:25 +0530

    Code

--- BankingTransactionModule/obj/Debug/net10.0/BankingTransactionModule1.AssemblyInfo.cs
+++ BankingTransactionModule/obj/Debug/net10.0/BankingTransactionModule1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BankingTransactionModule1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65c93270d806ea157c7d3213aff927659ae9a196")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2035cae49928803714f68c48fbbe3185fa08c7e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BankingTransactionModule1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BankingTransactionModule1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
